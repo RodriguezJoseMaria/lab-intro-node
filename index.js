96933392f78e836564471531eb976a7adf74a6ac
RodriguezJoseMaria <143606586+RodriguezJoseMaria@users.noreply.github.com>
2023-11-11 09:05:26 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+ this.items = [];
+ this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  };
 
-  get(pos) {}
+  get(pos) {
+  if (!this.items[pos]) {
+    throw new Error ("OutOfBounds");
+  } else {
+    return this.items[pos];
+  }
+}
 
-  max() {}
+  max() {
+    if (this.length > 0) {
+      return this.items[this.length - 1];
+  } 
+  else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length > 0) {
+      return this.items[0];
+  } 
+  else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
   sum() {}
 
